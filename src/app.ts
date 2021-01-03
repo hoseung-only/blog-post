c65335597cb1c08837550bf2348cf2775e7e0d5e
HoseungJang <hsjang.dev@gmail.com>
2021-01-03 23:49:35 +0900

    apply routers

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,9 @@
 import * as express from "express";
-import { Request, Response, NextFunction } from "express";
+
+import { routers } from "./routers";
 
 const app = express();
 
-app.get("/hello", (req: Request, res: Response) => {
-  return res.status(200).json({
-    message: "world",
-  });
-});
-
-app.get("/error", (req: Request, res: Response, next: NextFunction) => {
-  next(new Error);
-});
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  const err = new Error("Not Found");
-  next(err);
-});
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err.message === "Not Found") {
-    return res.status(404).json({
-      message: err.message,
-    });
-  } else {
-    return res.status(500).json({
-      message: "Internal Server Error",
-    });
-  }
-});
+app.use("/", routers());
 
 export { app };
