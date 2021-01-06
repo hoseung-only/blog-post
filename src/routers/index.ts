86e08dc9ae913e72bf27fa379ae9adae09ba737d
HoseungJang <hsjang.dev@gmail.com>
2021-01-06 23:43:42 +0900

    remove test handlers

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 
-import { applyTestHandlers } from "./test";
 import { applyErrorHandlers } from "./error";
 
 export const getRootRouter = () => {
   const router = Router();
 
-  applyTestHandlers(router);
   applyErrorHandlers(router);
 
   return router;
