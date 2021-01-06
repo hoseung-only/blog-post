7155e1ecd141618201224bcf7e92c3d9106f6f12
HoseungJang <hsjang.dev@gmail.com>
2021-01-07 00:31:45 +0900

    add paratemers validator

---
+++ src/routers/middlewares/validateParameters.ts
@@ -0,0 +1,17 @@
+import { RequestHandler } from "express";
+import { validationResult } from "express-validator";
+
+export const validateParameters: RequestHandler = (req, res, next) => {
+  const result = validationResult(req);
+
+  // if there were something error with parameters validation
+  if (!result.isEmpty()) {
+    // return all those errors with 400
+    return res.status(400).json({
+      messages: result.array(),
+    });
+  }
+
+  // if not, move to the next middleware.
+  next();
+};
