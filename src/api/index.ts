72ac76d7b6fbe573b566784625dde1d6111504eb
HoseungJang <hsjang.dev@gmail.com>
2021-01-12 01:15:53 +0900

    add category api handler WIP

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
 import { applyPostRouters } from "./routers/post";
+import { applyCategoryRouters } from "./routers/category";
 import { applyErrorHandlers } from "./errorHandlers";
 
 export const getRootRouter = () => {
   const router = Router();
 
   applyPostRouters(router);
+  applyCategoryRouters(router);
   applyErrorHandlers(router);
 
   return router;
---
+++ src/api/routers/category.ts
@@ -0,0 +1,65 @@
+import { Router } from "express";
+import { body, param } from "express-validator";
+
+import { validateParameters } from "../middlewares/validateParameters";
+
+import { Category } from "../../database/entities/category";
+
+export const applyCategoryRouters = (rootRouter: Router) => {
+  const router = Router();
+
+  router.post(
+    "/",
+    body("name")
+      .isString()
+      .withMessage("name must be string")
+      .exists()
+      .withMessage("name must be provided"),
+    body("parentId")
+      .isString()
+      .withMessage("parentId must be string")
+      .optional(),
+    validateParameters,
+    async (req, res, next) => {
+      try {
+        const name = req.body.name as string;
+        const parentId = req.body.parentId as string | undefined;
+
+        const result = await Category.create({ name, parentId });
+
+        return res.status(201).json({
+          result,
+        });
+      } catch (error) {
+        console.error(error);
+        return next(error);
+      }
+    }
+  );
+
+  router.get(
+    "/:id",
+    param("id")
+      .isString()
+      .withMessage("id must be string")
+      .exists()
+      .withMessage("id must be provided"),
+    validateParameters,
+    async (req, res, next) => {
+      try {
+        const id = req.params.id as string;
+
+        const result = await Category.findById(id);
+
+        return res.status(200).json({
+          result,
+        });
+      } catch (error) {
+        console.error(error);
+        return next(error);
+      }
+    }
+  );
+
+  rootRouter.use("/category", router);
+};
