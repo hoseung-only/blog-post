91e28eeba637c50aace6755068c1d8eb660bb769
HoseungJang <hsjang.dev@gmail.com>
2021-02-20 02:21:22 +0900

    add testcases for POST requests

--- src/__test__/category.spec.ts
+++ src/__test__/category.spec.ts
@@ -7,11 +7,10 @@ import { app } from "../app";
 import { Category } from "../database/entities/category";
 
 describe("Category Routers", () => {
-  let categoryId: number;
+  let parentCategory: Category;
 
   before(async () => {
-    const { id } = await Category.create({ name: "parent category" });
-    categoryId = id;
+    parentCategory = await Category.create({ name: "parent category" });
   });
 
   after(async () => {
@@ -22,14 +21,12 @@ describe("Category Routers", () => {
     context("When user has created category", () => {
       it("should return requested category", async () => {
         return request(app)
-          .get(`/category/${categoryId}`)
+          .get(`/category/${parentCategory.id}`)
           .expect(200)
           .then((response) => {
             const { result } = response.body;
 
-            expect(result).to.be.deep.contain({
-              name: "parent category",
-            });
+            expect(result).to.be.deep.eq(parentCategory);
           })
           .catch((error) => {
             throw error;
@@ -45,7 +42,10 @@ describe("Category Routers", () => {
       before(async () => {
         const results = await Promise.all(
           _.times(5, () =>
-            Category.create({ name: "child category", parentId: categoryId })
+            Category.create({
+              name: "child category",
+              parentId: parentCategory.id,
+            })
           )
         );
 
@@ -58,7 +58,7 @@ describe("Category Routers", () => {
 
       it("should return requested category", async () => {
         return request(app)
-          .get(`/category/${categoryId}/children`)
+          .get(`/category/${parentCategory.id}/children`)
           .expect(200)
           .then((response) => {
             const { result } = response.body;
@@ -68,22 +68,27 @@ describe("Category Routers", () => {
               {
                 id: 2,
                 name: "child category",
+                parentId: parentCategory.id,
               },
               {
                 id: 3,
                 name: "child category",
+                parentId: parentCategory.id,
               },
               {
                 id: 4,
                 name: "child category",
+                parentId: parentCategory.id,
               },
               {
                 id: 5,
                 name: "child category",
+                parentId: parentCategory.id,
               },
               {
                 id: 6,
                 name: "child category",
+                parentId: parentCategory.id,
               },
             ]);
           })
@@ -96,7 +101,7 @@ describe("Category Routers", () => {
     context("When requested category doesn't has any child categories", () => {
       it("should return requested category", async () => {
         return request(app)
-          .get(`/category/${categoryId}/children`)
+          .get(`/category/${parentCategory.id}/children`)
           .expect(200)
           .then((response) => {
             const { result } = response.body;
@@ -108,5 +113,98 @@ describe("Category Routers", () => {
           });
       });
     });
+  });
+
+  describe("POST / : create category", () => {
+    context("When user requests", () => {
+      it("should create a new one and return the result of it", async () => {
+        return request(app)
+          .post("/category")
+          .send({ name: "category" })
+          .expect(201)
+          .then((response) => {
+            const { result } = response.body;
+
+            expect(result).to.be.deep.contains({ name: "category" });
+          })
+          .catch((error) => {
+            throw error;
+          });
+      });
+    });
+
+    context("When user requests with the id of parent category", () => {
+      it("should create a new one and return the result of it", async () => {
+        return request(app)
+          .post("/category")
+          .send({ name: "category", parentId: parentCategory.id })
+          .expect(201)
+          .then((response) => {
+            const { result } = response.body;
+
+            expect(result).to.be.deep.contains({
+              name: "category",
+              parent: parentCategory,
+            });
+          })
+          .catch((error) => {
+            throw error;
+          });
+      });
+    });
+
+    context("When user requests with the invalid id of parent category", () => {
+      it("should return error response", async () => {
+        return request(app)
+          .post("/category")
+          .send({ name: "category", parentId: 12345 })
+          .expect(400)
+          .then((response) => {
+            const { message } = response.body;
+
+            expect(message).to.be.eq("Provided parent category does not exist");
+          })
+          .catch((error) => {
+            throw error;
+          });
+      });
+    });
+
+    context(
+      "When user requests with the id of parent category which already has parent",
+      () => {
+        let parentId: number;
+
+        before(async () => {
+          const { id } = await Category.create({
+            name: "parent category",
+            parentId: parentCategory.id,
+          });
+
+          parentId = id;
+        });
+
+        after(async () => {
+          await Category.deleteByIds([parentId]);
+        });
+
+        it("should return error response", async () => {
+          return request(app)
+            .post("/category")
+            .send({ name: "category", parentId })
+            .expect(400)
+            .then((response) => {
+              const { message } = response.body;
+
+              expect(message).to.be.eq(
+                "Depth of categories have to be up to 2"
+              );
+            })
+            .catch((error) => {
+              throw error;
+            });
+        });
+      }
+    );
   });
 });
