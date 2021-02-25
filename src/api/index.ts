c1f75d1600a602e8e4d18b38f04d9ccd1014986f
HoseungJang <hsjang.dev@gmail.com>
2021-02-25 22:04:32 +0900

    remove test router

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 
-import { applyTestRouters } from "./routers/test";
 import { applyPostRouters } from "./routers/post";
 import { applyCategoryRouters } from "./routers/category";
 import { applyErrorHandlers } from "./errorHandlers";
@@ -8,7 +7,6 @@ import { applyErrorHandlers } from "./errorHandlers";
 export const getRootRouter = () => {
   const router = Router();
 
-  applyTestRouters(router);
   applyPostRouters(router);
   applyCategoryRouters(router);
   applyErrorHandlers(router);
