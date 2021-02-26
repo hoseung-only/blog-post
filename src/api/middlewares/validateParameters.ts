58b8cc160728cfc1a9c4d71010b6207473fd0fc9
HoseungJang <hsjang.dev@gmail.com>
2021-02-26 22:56:23 +0900

    remove comments

--- src/api/middlewares/validateParameters.ts
+++ src/api/middlewares/validateParameters.ts
@@ -6,9 +6,7 @@ import { ErrorResponse } from "../../utils/error";
 export const validateParameters: RequestHandler = (req, res, next) => {
   const result = validationResult(req);
 
-  // if there were something error with parameters validation
   if (!result.isEmpty()) {
-    // return all those errors with 400
     const error = new ErrorResponse(
       400,
       result
@@ -19,6 +17,5 @@ export const validateParameters: RequestHandler = (req, res, next) => {
     return next(error);
   }
 
-  // if not, move to the next middleware.
   return next();
 };
